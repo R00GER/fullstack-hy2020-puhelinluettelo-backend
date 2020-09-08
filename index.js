978745edd716934d99d05fdbb57fd1508133e500
R00GER <roni.friman@sonit.fi>
2020-09-08 12:19:19 +0300

    Fix database bug

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
@@ -126,6 +127,17 @@ const errorHandler = (err, req, res, next) => {
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
+const url = process.env.MONGODB_URI;
+
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log(url);
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    console.log("error connecting to database:", error.message);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
--- models/person.js
+++ models/person.js
@@ -5,17 +5,6 @@ var uniqueValidator = require("mongoose-unique-validator");
 mongoose.set("useFindAndModify", false);
 mongoose.set('useCreateIndex', true);
 
-const url = process.env.MONGODB_URI;
-
-mongoose
-  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Connected to database");
-  })
-  .catch((error) => {
-    console.log("error connecting to database:", error.message);
-  });
-
 const personSchema = new mongoose.Schema({
   name: { type: String, minlength: 3, required: true, unique: true },
   number: { type: Number, validate: /[0-9]{8,}/, required: true },
